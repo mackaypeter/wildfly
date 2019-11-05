5daaa209af0e3282ff2411c63bb7c1703907518e
Peter Mačkay <pmackay@redhat.com>
2019-11-05 15:33:24 +0100

    [JBEAP-17802] Add test for property replacement in beans.xml

--- testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/weld/alternative/WeldAlternativeTestCase.java
+++ testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/weld/alternative/WeldAlternativeTestCase.java
@@ -24,7 +24,13 @@ package org.jboss.as.test.integration.weld.alternative;
 import javax.inject.Inject;
 
 import org.jboss.arquillian.container.test.api.Deployment;
+//import org.jboss.arquillian.container.test.api.OperateOnDeployment;
+import org.jboss.arquillian.container.test.api.OperateOnDeployment;
 import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.as.arquillian.api.ServerSetup;
+import org.jboss.as.arquillian.api.ServerSetupTask;
+import org.jboss.as.arquillian.container.ManagementClient;
+import org.jboss.dmr.ModelNode;
 import org.jboss.shrinkwrap.api.Archive;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.StringAsset;
@@ -33,21 +39,43 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.NAME;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.OP;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.OP_ADDR;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.SUBSYSTEM;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.VALUE;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.WRITE_ATTRIBUTE_OPERATION;
+
 /**
  *
  * A test of the CDI alternatives. This tests that the alternative
  * information in the beans.xml file is being parsed correctly.
  *
  * @author Stuart Douglas
+ * @author Peter Mackay
  */
 @RunWith(Arquillian.class)
+@ServerSetup(WeldAlternativeTestCase.WeldAlternativeTestCaseServerSetup.class)
 public class WeldAlternativeTestCase {
+
+    public static final String SUBSTITUTION_DEPLOYMENT = "substitutionDeployment";
 
     @Deployment
     public static Archive<?> deploy() {
-        JavaArchive jar = ShrinkWrap.create(JavaArchive.class);
-        jar.addPackage(WeldAlternativeTestCase.class.getPackage());
-        jar.addAsManifestResource(new StringAsset("<beans><alternatives><class>" + AlternativeBean.class.getName() + "</class></alternatives></beans>"), "beans.xml");
+        return getDeployment(false);
+    }
+
+    @Deployment(name = SUBSTITUTION_DEPLOYMENT)
+    public static Archive<?> deployWithSubstitution() {
+        return getDeployment(true);
+    }
+
+    public static Archive<?> getDeployment(boolean substitution) {
+        String alternativeClass = substitution ? "${alt.bean:" + AlternativeBean.class.getName() + "}" : AlternativeBean.class.getName();
+        String beansXmlString = "<beans><alternatives><class>" + alternativeClass + "</class></alternatives></beans>";
+        JavaArchive jar = ShrinkWrap.create(JavaArchive.class)
+                .addPackage(WeldAlternativeTestCase.class.getPackage())
+                .addAsManifestResource(new StringAsset(beansXmlString), "beans.xml");
         return jar;
     }
 
@@ -56,8 +84,39 @@ public class WeldAlternativeTestCase {
 
     @Test
     public void testAlternatives() {
+        testAlternative();
+    }
+
+    @Test
+    @OperateOnDeployment(SUBSTITUTION_DEPLOYMENT)
+    public void testAlternativesWithSubstitution() {
+        testAlternative();
+    }
+
+    private void testAlternative() {
         Assert.assertEquals("Hello World", bean.sayHello());
     }
 
+    public static class WeldAlternativeTestCaseServerSetup implements ServerSetupTask {
 
+        @Override
+        public void setup(final ManagementClient managementClient, final String containerId) throws Exception {
+            final ModelNode op = new ModelNode();
+            op.get(OP_ADDR).set(SUBSYSTEM, "ee");
+            op.get(OP).set(WRITE_ATTRIBUTE_OPERATION);
+            op.get(NAME).set("spec-descriptor-property-replacement");
+            op.get(VALUE).set(true);
+            managementClient.getControllerClient().execute(op);
+        }
+
+        @Override
+        public void tearDown(final ManagementClient managementClient, final String containerId) throws Exception {
+            final ModelNode op = new ModelNode();
+            op.get(OP_ADDR).set(SUBSYSTEM, "ee");
+            op.get(OP).set(WRITE_ATTRIBUTE_OPERATION);
+            op.get(NAME).set("spec-descriptor-property-replacement");
+            op.get(VALUE).set(false);
+            managementClient.getControllerClient().execute(op);
+        }
+    }
 }
